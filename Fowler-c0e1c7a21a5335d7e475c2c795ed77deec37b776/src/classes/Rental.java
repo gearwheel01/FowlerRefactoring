203b3d3c45a7b67c94303f00635fbe8a2982f893
hudla <hudla01@gmx.de>
2021-05-09 17:01:00 +0200

    moved amountFor method to rental

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/classes/Rental.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/classes/Rental.java
@@ -13,4 +13,25 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    public double getCharge() {
+        double result = 0;
+
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
